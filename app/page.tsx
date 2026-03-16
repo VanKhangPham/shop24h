830847eea053a8ea25ca03d6bfa993c5e18b548b
VanKhangPham <phamvankhang182004@gmail.com>
2026-03-16 09:58:18 +0700

    update components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+
 
 const Home = () => {
   return (
     <div className='p-10 bg-shop-light-pink'>
       <h2 className='text-xl font-semibold'>Home</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis veniam error nisi esse numquam assumenda sequi perferendis et cum natus! Eum, voluptates harum! Praesentium, cum ipsa. Totam cupiditate id architecto voluptas repellendus possimus unde doloremque sit facere perferendis veritatis accusamus aliquam obcaecati amet harum laboriosam accusantium eveniet, minus sunt. Commodi.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis veniam error nisi esse numquam assumenda sequi perferendis et cum natus! Eum, voluptates harum! Praesentium, cum ipsa. Totam cupiditate id architecto voluptas repellendus possimus unde doloremque sit facere perferendis veritatis accusamus aliquam obcaecati amet harum laboriosam accusantium eveniet, minus sunt. Commodi.
+      </p>
+      <Button size="lg">Checkout</Button>
     </div>
   )
 }
