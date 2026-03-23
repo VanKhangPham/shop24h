0e5c5161a95591340ae67e07c9681abc8c9d0d07
VanKhangPham <phamvankhang182004@gmail.com>
2026-03-23 23:10:47 +0700

    create footer

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,13 +2,32 @@ import React from 'react'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
 
+type LogoProps = {
+  className?: string
+  spanDesign?: string
+}
 
-const Logo = ({clasName, spanDesign}: {clasName?:string, spanDesign?:string}) => {
-  return <Link href={"/"}>
-    <h2 className={cn("text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans",clasName)}>
-        Shop24<span className={cn("text-shop_light_green group-hover:text-shop_dark_green hoverEffect", spanDesign)}>h</span>
-    </h2>
-  </Link>
+const Logo = ({ className, spanDesign }: LogoProps) => {
+  return (
+    <Link href={"/"} className="inline-flex">
+      <h2
+        className={cn(
+          "text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans",
+          className
+        )}
+      >
+        Shop24
+        <span
+          className={cn(
+            "text-shop_light_green group-hover:text-shop_dark_green hoverEffect",
+            spanDesign
+          )}
+        >
+          h
+        </span>
+      </h2>
+    </Link>
+  )
 }
 
 export default Logo
