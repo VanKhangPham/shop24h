6f959f3bda123660c5867f0b16d59198b0f88ad0
VanKhangPham <phamvankhang182004@gmail.com>
2026-03-24 22:39:28 +0700

    create_cms

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import {ClerkProvider} from "@clerk/nextjs";
+
+// const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - Shop24h online store",
+    default: "Shop24h online store",
+  },
+  description:
+    "Shop24h online store, Your one stop shop for all your needs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+            <main className="flex-1">{children}</main>
+          <Footer/>
+        </div>
+    </ClerkProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,8 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import {ClerkProvider} from "@clerk/nextjs";
-
-// const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s - Shop24h online store",
-    default: "Shop24h online store",
-  },
-  description:
-    "Shop24h online store, Your one stop shop for all your needs",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
-       <html lang="en">
-      <body className="font-poppins antialiased">
-        <div className="flex flex-col min-h-screen">
-          <Header />
-            <main className="flex-1">{children}</main>
-          <Footer/>
-        </div>
-      </body>
+    <html lang="en">
+      <body className="font-poppins antialiased">{children}</body>
     </html>
-    </ClerkProvider>
   );
-}
+};
+export default RootLayout;
